96b1f8596ea5ada30ba91f563de138c1dbbe3473
SergeiBak <77221025+SergeiBak@users.noreply.github.com>
2021-12-24 23:43:18 -0800

    Setup pacman input + rotation

--- projectCode/Pac-Man/Assets/Scripts/Pacman.cs
+++ projectCode/Pac-Man/Assets/Scripts/Pacman.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Pacman : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Movement movement { get; private set; }
+
+    private void Awake()
     {
-        
+        movement = GetComponent<Movement>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            movement.SetDirection(Vector2.up);
+        }
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            movement.SetDirection(Vector2.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            movement.SetDirection(Vector2.down);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            movement.SetDirection(Vector2.right);
+        }
+
+        float angle = Mathf.Atan2(movement.direction.y, movement.direction.x); // get angle based on direction
+        transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
     }
 }
