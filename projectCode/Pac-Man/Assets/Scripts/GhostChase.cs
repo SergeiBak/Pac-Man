af527b2a7850ec41af2c46416a1e480eb22fd344
SergeiBak <77221025+SergeiBak@users.noreply.github.com>
2021-12-25 20:14:22 -0800

    Completed Chase State
    Completed Chase State for ghost, in which the ghost will try and chase the player based on the shortest possible path

--- projectCode/Pac-Man/Assets/Scripts/GhostChase.cs
+++ projectCode/Pac-Man/Assets/Scripts/GhostChase.cs
@@ -2,7 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GhostChase : GhostBehavior
+public class GhostChase : GhostBehavior // chase after player, when chase time is up transition to scatter state
 {
+    private void OnDisable() // transistion to scatter state
+    {
+        ghost.scatter.Enable();
+    }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Node node = collision.GetComponent<Node>();
+
+        if (node != null && this.enabled && !ghost.frightened.enabled)
+        {
+            Vector2 direction = Vector2.zero;
+            float minDistance = float.MaxValue;
+
+            foreach (Vector2 availableDirection in node.availableDirections)
+            {
+                Vector3 newPosition = transform.position + new Vector3(availableDirection.x, availableDirection.y, 0.0f);
+                float distance = (ghost.target.position - newPosition).sqrMagnitude; // calculate distance from possible direction to target being chased. sqrMagnitude better for performance than magnitude
+
+                if (distance < minDistance) // if this path is shorter, set direction to this direction
+                {
+                    direction = availableDirection;
+                    minDistance = distance;
+                }
+            }
+
+            ghost.movement.SetDirection(direction);
+        }
+    }
 }
