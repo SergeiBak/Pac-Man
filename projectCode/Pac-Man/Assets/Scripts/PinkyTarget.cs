11337001b68ba1ba2ece42f6f30c9a617ea1f778
SergeiBak <77221025+SergeiBak@users.noreply.github.com>
2021-12-28 13:13:22 -0800

    Setup Pinky Target
    Pinky Chase State fully updated, her target position can be made visible through toggling with 'P' key. In addition, the overflow bug from the original pacman is simulated where her target pos is slightly off when pacman is facing up. This bug is set to true by default, but can be toggled using 'O' key

--- projectCode/Pac-Man/Assets/Scripts/PinkyTarget.cs
+++ projectCode/Pac-Man/Assets/Scripts/PinkyTarget.cs
@@ -4,15 +4,93 @@ using UnityEngine;
 
 public class PinkyTarget : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Pacman pacman;
+
+    private bool visible = false;
+    private bool overflowBug = true;
+
+    private SpriteRenderer sr;
+
+    private void Awake()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        CheckVisiblity();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ToggleVisiblity();
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            ToggleOverflowBug();
+        }
+
+        UpdatePosition();
+    }
+
+    private void ToggleVisiblity() // toggles visible boolean and runs a check
+    {
+        visible = !visible;
+
+        CheckVisiblity();
+    }
+
+    private void ToggleOverflowBug() // toggles overflow bug, if true, then simulates the overflow bug in the original pacman where when pacman is facing up the target is also 4 units left of him
+    {
+        overflowBug = !overflowBug;
+    }
+
+    private void CheckVisiblity() // updates sprite visibility based on visible boolean
+    {
+        if (visible)
+        {
+            sr.enabled = true;
+        }
+        else
+        {
+            sr.enabled = false;
+        }
+    }
+
+    private void UpdatePosition() // moves target position to 4 tiles ahead of pacman's position
+    {
+        Vector3 newPostion = pacman.gameObject.transform.position; // get pacman's position
+        newPostion.z = transform.position.z; // make sure z value of target not affected
+
+        if (pacman.movement.direction == Vector2.up)
+        {
+            newPostion.y += 4;
+
+            if (overflowBug)
+            {
+                newPostion.x -= 4;
+            }
+
+            transform.position = newPostion;
+        }
+        else if (pacman.movement.direction == Vector2.down)
+        {
+            newPostion.y -= 4;
+            transform.position = newPostion;
+        }
+        else if (pacman.movement.direction == Vector2.left)
+        {
+            newPostion.x -= 4;
+            transform.position = newPostion;
+        }
+        else if (pacman.movement.direction == Vector2.right)
+        {
+            newPostion.x += 4;
+            transform.position = newPostion;
+        }
     }
 }
