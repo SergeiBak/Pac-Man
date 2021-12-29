ff8cbc07a141782cd3eda8bdc4da4a2d2ad5df4d
SergeiBak <77221025+SergeiBak@users.noreply.github.com>
2021-12-28 19:42:42 -0800

    Setup Clyde Target
    Clyde Chase State fully updated, his target position can be made visible through toggling with 'C' key.

--- projectCode/Pac-Man/Assets/Scripts/ClydeTarget.cs
+++ projectCode/Pac-Man/Assets/Scripts/ClydeTarget.cs
@@ -4,15 +4,85 @@ using UnityEngine;
 
 public class ClydeTarget : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Transform pacman;
+    [SerializeField]
+    private Transform bottomLeftCorner;
+    [SerializeField]
+    private Ghost clyde;
+
+    private bool visible = false;
+
+    private SpriteRenderer sr;
+
+    [SerializeField]
+    private LineRenderer lr;
+
+    private void Awake()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
+        CheckVisiblity();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ToggleVisiblity();
+        }
+
+        UpdatePosition();
+    }
+
+    private void ToggleVisiblity() // toggles visible boolean and runs a check
+    {
+        visible = !visible;
+
+        CheckVisiblity();
+    }
+
+    private void CheckVisiblity() // updates sprite visibility based on visible boolean
+    {
+        if (visible)
+        {
+            sr.enabled = true;
+            lr.enabled = true;
+        }
+        else
+        {
+            sr.enabled = false;
+            lr.enabled = false;
+        }
+    }
+
+    private void UpdatePosition() // moves target position to pacman's position, if within 8 tiles then retreat to corner
+    {
+        float distance = Vector2.Distance(pacman.position, clyde.gameObject.transform.position); // get distance between pacman & Clyde
+
+        Vector3 newPostion = new Vector3();
+
+        if (distance > 8)
+        {
+            newPostion = pacman.position; // pacman is target
+        }
+        else
+        {
+            newPostion = bottomLeftCorner.position; // Clyde's corner is target
+        }
         
+        newPostion.z = transform.position.z; // make sure z value of target not affected
+        transform.position = newPostion;
+
+        UpdateLine(clyde.gameObject.transform.position, newPostion);
+    }
+
+    private void UpdateLine(Vector3 start, Vector3 end)
+    {
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
     }
 }
